240b63f7db595e0dbe49044aee2813e78d7108bd
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 14:32:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome #{name}. You are number #{katz_deli.size} in line"
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)

d41025ade3afcc6cf9adf28e01b6c8cd60092974
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 14:30:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  var queue = []
-  for
-  if katz_deli.length > 0
-    puts "The line is currently "
-  else
-    puts "The line is currently empty."
-  end
+
 end
 
 def take_a_number(katz_deli, name)

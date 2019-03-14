ca473186be0b66dee6275f143ddc55f56d06af8d
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 14:29:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,13 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome #{name}. You are number #{katz_deli.size} in line"
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end

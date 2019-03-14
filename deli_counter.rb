17b8e170b7628057189adc66c707e5e04ddf7b88
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 14:26:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,15 @@
 katz_deli = []
 
 def line(katz_deli)
+  var queue = []
+  for
   if katz_deli.length > 0
-    return katz_deli
+    puts "The line is currently "
   else
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome #{name}. You are number #{katz_deli.size} in line"

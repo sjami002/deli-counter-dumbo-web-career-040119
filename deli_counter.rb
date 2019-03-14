54db1d8564b49c6f92c82208f8871f34c0a5b661
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 14:38:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    number = 1
+    num = 1
     katz_deli.each do |customer|
-      queue << "#{number}. #{customer}"
-      number += 1
+      queue << "#{num}. #{customer}"
+      num += 1
     end
     puts "The line is currently: #{queue.join(" ")}"
   end

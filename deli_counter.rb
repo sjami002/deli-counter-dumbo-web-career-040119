2333746c8a52e86c304aeec2fe2e763cbb02cf09
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 14:36:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,16 @@
 katz_deli = []
 
 def line(katz_deli)
-
+  queue = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      queue << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently #{queue.join(", ")}"
 end
 
 def take_a_number(katz_deli, name)
